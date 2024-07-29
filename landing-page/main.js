bb539e6470bea9a0865f5cadacb87f4ca3736436
almashruf <tonoy5300@gmail.com>
2024-07-29 22:15:11 +0600

    lec - 4,5

--- landing-page/main.js
+++ landing-page/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const primaryHeader = document.querySelector('.primary-header')
+const navtoggel = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
-setupCounter(document.querySelector('#counter'))
+navToggle.addEventListener("click", ()=>{
+  primaryNav.hasAttribute("data-visible")
+  ? navToggle.setAttribute("aria-exoanded", false)
+  : navToggle.setAttribute("aria-exoanded", true)
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute('data-overlay')
+})
